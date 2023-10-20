d10fc00651dde827deaf2845c9d408da243866d6
Shingyu Kang <rkdtlseb@naver.com>
2023-10-20 15:36:31 +0900

    second commit upload spring

---
+++ src/main/java/com/projectss/theUltimateTodo/TheUltimateTodoApplication.java
@@ -0,0 +1,13 @@
+package com.projectss.theUltimateTodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheUltimateTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheUltimateTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectss/theUltimateTodo/TheUltimateTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectss.theUltimateTodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheUltimateTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
