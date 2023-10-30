d85dc0631f08ecd0be3f12e52799448f877a5d35
Shingyu Kang <rkdtlseb@naver.com>
2023-10-30 12:46:08 +0900

    [feat] 로그파일 만들고, application.properties 수정해서 파일에 로그 저장하게끔함 + test endpoint 추가

--- src/main/java/com/projectss/theUltimateTodo/Controller/TestController.java
+++ src/main/java/com/projectss/theUltimateTodo/Controller/TestController.java
@@ -1,12 +1,21 @@
 package com.projectss.theUltimateTodo.Controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class TestController {
     @GetMapping("/test")
-    public String test() {
+    public String test(@RequestBody String body) {
+        log.info("TestController.test() called");
+        return "Hello World!";
+    }
+    @GetMapping("/test2")
+    public String test2() {
+        log.info("TestController.test2() called");
         return "Hello World!";
     }
 }
