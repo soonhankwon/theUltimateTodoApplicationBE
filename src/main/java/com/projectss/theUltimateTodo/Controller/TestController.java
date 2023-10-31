8543125894ae7d61a296e0898da932788418bbd8
rkdtlseb <rkdtlseb@naver.com>
2023-10-31 10:12:00 +0900

    []

--- src/main/java/com/projectss/theUltimateTodo/Controller/TestController.java
+++ src/main/java/com/projectss/theUltimateTodo/Controller/TestController.java
@@ -13,11 +13,7 @@ public class TestController {
     public String main(){
         return "hello world!";
     }
-    @GetMapping("/test")
-    public String test(@RequestBody String body) {
-        log.info("TestController.test() called");
-        return "Hello World!";
-    }
+
     @GetMapping("/test2")
     public String test2() {
         log.info("TestController.test2() called");
