4d34b2bbcae17275a36d023a19fad14a9ea97b47
Shingyu Kang <rkdtlseb@naver.com>
2023-11-14 19:37:00 +0900

    [feature] add login controller

---
+++ src/main/java/com/projectss/theUltimateTodo/controller/OAuthController.java
@@ -0,0 +1,13 @@
+package com.projectss.theUltimateTodo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class OAuthController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+}
