1c4d7185526dc94b1a0916039c3cd3546f8c8075
Shingyu Kang <rkdtlseb@naver.com>
2023-11-16 16:21:58 +0900

    [fix] ProxyConfig

--- src/main/java/com/projectss/theUltimateTodo/OAuth/OpenApiController.java
+++ src/main/java/com/projectss/theUltimateTodo/OAuth/OpenApiController.java
@@ -4,10 +4,13 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
