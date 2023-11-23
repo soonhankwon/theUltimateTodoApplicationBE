1c059896fc7528163dc00f0c96feae99b8ab0ea4
soonhankwon <soonable@gmail.com>
2023-11-23 18:57:29 +0900

    feat: @AuthenticationPrincipal 적용 & 리팩토링

--- src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoController.java
+++ src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoController.java
@@ -1,12 +1,15 @@
 package com.projectss.theUltimateTodo.memo.controller;
 
+import com.projectss.theUltimateTodo.OAuth.SecurityUser;
 import com.projectss.theUltimateTodo.memo.dto.MemoRequest;
 import com.projectss.theUltimateTodo.memo.service.MemoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,65 +20,72 @@ public class MemoController {
 
     private final MemoService memoService;
 
-    @Operation(summary = "최상위 루트 메모 저장 API", description = "루트 메모 저장 & 업데이트")
+    @Operation(summary = "최상위 루트 메모 저장 API", description = "루트 메모 저장 & 업데이트", responses = @ApiResponse(responseCode = "201"))
     @PostMapping
-    public ResponseEntity<?> createRootMemo(@RequestBody MemoRequest dto) {
-        String loginUserEmail = "test@naver.com";
-        memoService.createRootMemo(loginUserEmail, dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body("success");
+    public ResponseEntity<String> createRootMemo(@AuthenticationPrincipal SecurityUser securityUser,
+                                                 @RequestBody MemoRequest dto) {
+        String email = securityUser.getUsername();
+        memoService.createRootMemo(email, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("created");
     }
 
-    @Operation(summary = "디렉토리 안에 메모 저장", description = "디렉토리 하위 메모 저장")
+    @Operation(summary = "디렉토리 안에 메모 저장", description = "디렉토리 하위 메모 저장", responses = @ApiResponse(responseCode = "201"))
     @PostMapping("/{directoryId}")
-    public ResponseEntity<?> createMemoInDirectory(@PathVariable String directoryId,
-                                                   @RequestBody MemoRequest dto) {
-        String loginUserEmail = "test@naver.com";
-        memoService.createMemoInDirectory(loginUserEmail, directoryId, dto);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> createMemoInDirectory(@AuthenticationPrincipal SecurityUser securityUser,
+                                                        @PathVariable String directoryId,
+                                                        @RequestBody MemoRequest dto) {
+        String email = securityUser.getUsername();
+        memoService.createMemoInDirectory(email, directoryId, dto);
+        return ResponseEntity.ok().body("created");
     }
 
     @Operation(summary = "메모 업데이트 API")
     @PatchMapping("/{memoId}")
-    public ResponseEntity<?> updateMemo(@PathVariable String memoId,
-                                        @RequestBody MemoRequest dto) {
-        String loginUserEmail = "test@naver.com";
-        memoService.updateMemo(loginUserEmail, memoId, dto);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> updateMemo(@AuthenticationPrincipal SecurityUser securityUser,
+                                             @PathVariable String memoId,
+                                             @RequestBody MemoRequest dto) {
+        String email = securityUser.getUsername();
+        memoService.updateMemo(email, memoId, dto);
+        return ResponseEntity.ok().body("updated");
     }
 
     @Operation(summary = "루트 메모 위치 수정 API", description = "루트 메모 드래그 앤 드랍")
     @PutMapping("/{memoId}/{targetDirectoryId}")
-    public ResponseEntity<?> updateRootMemoLocation(@PathVariable String memoId,
-                                                @PathVariable String targetDirectoryId) {
-        String loginUserEmail = "test@naver.com";
-        memoService.moveRootMemoLocation(loginUserEmail, memoId, targetDirectoryId);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> updateRootMemoLocation(@AuthenticationPrincipal SecurityUser securityUser,
+                                                         @PathVariable String memoId,
+                                                         @PathVariable String targetDirectoryId) {
+        String email = securityUser.getUsername();
+        memoService.moveRootMemoLocation(email, memoId, targetDirectoryId);
+        return ResponseEntity.ok().body("updated");
     }
 
     @Operation(summary = "메모 to 디렉토리 위치 수정 API", description = "메모 드래그 앤 드랍")
     @PutMapping("/{memoId}/{parentDirectoryId}/{targetDirectoryId}")
-    public ResponseEntity<?> updateMemoLocationToDirectory(@PathVariable String memoId,
-                                                @PathVariable String parentDirectoryId,
-                                                @PathVariable String targetDirectoryId) {
-        String loginUserEmail = "test@naver.com";
-        memoService.moveLocationToDirectory(loginUserEmail, memoId, parentDirectoryId, targetDirectoryId);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> updateMemoLocationToDirectory(@AuthenticationPrincipal SecurityUser securityUser,
+                                                                @PathVariable String memoId,
+                                                                @PathVariable String parentDirectoryId,
+                                                                @PathVariable String targetDirectoryId) {
+        String email = securityUser.getUsername();
+        memoService.moveLocationToDirectory(email, memoId, parentDirectoryId, targetDirectoryId);
+        return ResponseEntity.ok().body("updated");
     }
 
     @Operation(summary = "메모 to 최상단 API API", description = "메모 to 최상단 드래그 앤 드랍")
     @PutMapping("/{memoId}/{parentDirectoryId}/root/move")
-    public ResponseEntity<?> updateMemoLocationToRoot(@PathVariable String memoId,
-                                                @PathVariable String parentDirectoryId) {
-        String loginUserEmail = "test@naver.com";
-        memoService.moveLocationToRoot(loginUserEmail, memoId, parentDirectoryId);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> updateMemoLocationToRoot(@AuthenticationPrincipal SecurityUser securityUser,
+                                                           @PathVariable String memoId,
+                                                           @PathVariable String parentDirectoryId) {
+        String email = securityUser.getUsername();
+        memoService.moveLocationToRoot(email, memoId, parentDirectoryId);
+        return ResponseEntity.ok().body("updated");
     }
 
     @Operation(summary = "메모 삭제 API")
     @DeleteMapping("/{memoId}")
-    public ResponseEntity<?> deleteMemo(@PathVariable String memoId) {
-        String loginUserEmail = "test@naver.com";
-        memoService.deleteMemo(loginUserEmail, memoId);
-        return ResponseEntity.ok().body("success");
+    public ResponseEntity<String> deleteMemo(@AuthenticationPrincipal SecurityUser securityUser,
+                                             @PathVariable String memoId) {
+        String email = securityUser.getUsername();
+        memoService.deleteMemo(email, memoId);
+        return ResponseEntity.ok().body("deleted");
     }
 }
--- src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoStoreController.java
+++ src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoStoreController.java
@@ -1,5 +1,6 @@
 package com.projectss.theUltimateTodo.memo.controller;
 
+import com.projectss.theUltimateTodo.OAuth.SecurityUser;
 import com.projectss.theUltimateTodo.memo.domain.MemoStore;
 import com.projectss.theUltimateTodo.memo.service.MemoStoreService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -7,6 +8,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +24,17 @@ public class MemoStoreController {
 
     @Operation(summary = "메모 최상위 루트(구조) 조회 API")
     @GetMapping
-    public ResponseEntity<MemoStore> getRootByUser() {
-        String loginUserEmail = "test@naver.com";
-        MemoStore memoStoreByUser = memoStoreService.getMemoStoreByUser(loginUserEmail);
+    public ResponseEntity<MemoStore> getRootByUser(@AuthenticationPrincipal SecurityUser securityUser) {
+        String email = securityUser.getUsername();
+        MemoStore memoStoreByUser = memoStoreService.getMemoStoreByUser(email);
         return ResponseEntity.ok().body(memoStoreByUser);
     }
 
     @Operation(summary = "메모 스토어 생성 API - 테스트용")
     @PostMapping
-    public ResponseEntity<?> createMemoStore() {
-        String loginUserEmail = "test@naver.com";
-        memoStoreService.createMemoStoreByUser(loginUserEmail);
+    public ResponseEntity<String> createMemoStore(@AuthenticationPrincipal SecurityUser securityUser) {
+        String email = securityUser.getUsername();
+        memoStoreService.createMemoStoreByUser(email);
         return ResponseEntity.status(HttpStatus.CREATED).body("ok");
     }
 }
