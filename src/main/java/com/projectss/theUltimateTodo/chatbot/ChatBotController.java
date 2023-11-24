e5eddec1f47f7494b90212b8b77a782c64d13189
Shingyu Kang <rkdtlseb@naver.com>
2023-11-24 15:10:53 +0900

    [feature] chat bot skill test

--- src/main/java/com/projectss/theUltimateTodo/chatbot/ChatBotController.java
+++ src/main/java/com/projectss/theUltimateTodo/chatbot/ChatBotController.java
@@ -2,10 +2,8 @@ package com.projectss.theUltimateTodo.chatbot;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,38 +11,18 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@Slf4j
 public class ChatBotController {
-    @RequestMapping(value = "/ChatBot/test" , method= {RequestMethod.POST , RequestMethod.GET },headers = {"Accept=application/json"})
-    public HashMap<String,Object> RSAPI(@RequestBody Map<String, Object> params, HttpServletRequest request, HttpServletResponse response) {
+    @GetMapping("/ChatBot/test")
+    public void chatbot(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) {
+      log.info(body);
+      log.info(request.toString());
 
-        HashMap<String, Object> resultJson = new HashMap<>();
+    }
+    @PostMapping("/ChatBot/test")
+    public void chatbotPost(@RequestBody String body, HttpServletRequest request, HttpServletResponse response) {
+        log.info(body);
+        log.info(request.toString());
 
-        try{
-            HashMap<String,Object> userRequest =  (HashMap<String,Object>)params.get("userRequest");
-
-            // 사용자가 입력한 채팅을 불러오는 부분
-            String utter = userRequest.get("utterance").toString().replace("\n","");
-            System.out.println(utter);
-
-            //별도 지정한 트리거 없이 채팅을 입력하면 메모가 저장되었습니다 라는 채팅 반환
-            List<HashMap<String,Object>> outputs = new ArrayList<>();
-            HashMap<String,Object> template = new HashMap<>();
-            HashMap<String, Object> simpleText = new HashMap<>();
-            HashMap<String, Object> text = new HashMap<>();
-
-            text.put("text","메모가 저장되었습니다.");
-            simpleText.put("simpleText",text);
-            outputs.add(simpleText);
-
-            template.put("outputs",outputs);
-
-            resultJson.put("version","2.0");
-            resultJson.put("template",template);
-
-        }catch (Exception e){
-
-        }
-
-        return resultJson;
     }
 }
