af2493d41a51602eb0897d144c67f1d44906e624
Shingyu Kang <rkdtlseb@naver.com>
2023-11-24 12:38:03 +0900

    [feature] referer 받아보기

--- src/main/java/com/projectss/theUltimateTodo/OAuth/OpenApiController.java
+++ src/main/java/com/projectss/theUltimateTodo/OAuth/OpenApiController.java
@@ -5,24 +5,22 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/openApi")
+@RequestMapping("/openApi/kakao")
 @Slf4j
 public class OpenApiController {
 
     private final OpenApiService openApiService;
 
-    @GetMapping("/kakao")
+    @GetMapping()
     public String kakao(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         String token  = openApiService.getToken(code);
         log.info("token : {} ",token );
@@ -30,5 +28,15 @@ public class OpenApiController {
         return token;
 //
     }
+    @PostMapping("callback")
+    public ResponseEntity<String> handleKakaoCallback(@RequestBody String callbackData) {
+        // 받은 콜백 데이터를 로그에 출력하거나 원하는 처리를 수행합니다.
+        log.info("Received KakaoTalk Callback Data:");
+        log.info(callbackData);
+
+        // 처리 완료 후 클라이언트에 응답을 반환합니다.
+        return new ResponseEntity<>("Callback data received successfully", HttpStatus.OK);
+    }
+
 
 }
