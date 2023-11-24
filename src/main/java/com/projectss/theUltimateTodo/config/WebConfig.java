d0a85b428d59862e524403d14180b704ba698445
Shingyu Kang <rkdtlseb@naver.com>
2023-11-24 15:38:47 +0900

    [feature] log request

--- src/main/java/com/projectss/theUltimateTodo/config/WebConfig.java
+++ src/main/java/com/projectss/theUltimateTodo/config/WebConfig.java
@@ -13,8 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 
         registry.addMapping("/**")
                 .allowCredentials(true)
-//                .allowedOrigins("http://localhost:5173","https://k28951c68ade3a.user-app.krampoline.com","https://memo-fe-woad.vercel.app")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:5173","https://k28951c68ade3a.user-app.krampoline.com","https://memo-fe-woad.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .maxAge(3600);
