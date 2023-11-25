b2629ffd53708ae8c73b248b38b6186b226a62e2
Shingyu Kang <rkdtlseb@naver.com>
2023-11-25 14:03:45 +0900

    [test] fallback 스킬테스트

--- src/main/java/com/projectss/theUltimateTodo/chatbot/ChatBotController.java
+++ src/main/java/com/projectss/theUltimateTodo/chatbot/ChatBotController.java
@@ -40,12 +40,12 @@ public class ChatBotController {
                 "        \"textCard\": {\n" +
                 "          \"title\": \"서비스를 이용하려면 회원가입이 필요합니다. \",\n" +
                 "          \"description\": \" 회원가입 시 이곳에 적는 글이 메모에 저장되어 PC에서도 확인하실 수 있습니다. \",\n" +
-                "          \"buttons\": [\n" +
-                "            {\n" +
-                "              \"action\": \"block\",\n" +
-                "              \"blockId\": \"656011bc501358649ef6974a\"\n" +
-                "            },\n" +
-                "          ]\n" +
+//                "          \"buttons\": [\n" +
+//                "            {\n" +
+//                "              \"action\": \"block\",\n" +
+//                "              \"blockId\": \"656011bc501358649ef6974a\"\n" +
+//                "            },\n" +
+//                "          ]\n" +
                 "        }\n" +
                 "      }\n" +
                 "    ]\n" +
