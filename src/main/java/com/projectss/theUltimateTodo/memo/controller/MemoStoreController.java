2f97bf8269725b12e61f51e20c1c4827ab58bb44
KangShinGyu98 <103213494+KangShinGyu98@users.noreply.github.com>
2023-11-27 12:19:13 +0900

    Update MemoStoreController.java

--- src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoStoreController.java
+++ src/main/java/com/projectss/theUltimateTodo/memo/controller/MemoStoreController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/memo-stores")
+@RequestMapping("/api/memo-stores2")
 @Tag(name = "메모 최상위 루트(구조) 조회 API")
 public class MemoStoreController {
 
